0b2e9ba3573d14b37a431198e420b6222e216525
Florian Gilcher <florian.gilcher@asquera.de>
2014-08-30 14:27:26 +0200

    Refactor server implementation

--- src/bin/drossel-server.rs
+++ src/bin/drossel-server.rs
@@ -7,9 +7,9 @@ extern crate rustuv;
 extern crate drossel;
 extern crate strand;
 
-use std::io::net::tcp::TcpListener;
+use std::io::net::tcp::{TcpListener,TcpStream};
 use std::io::{Acceptor, Listener};
-use std::io::BufferedStream;
+use std::io::{BufferedStream,Stream};
 use drossel::commands::util::get_command;
 use drossel::commands::command::Command;
 use drossel::drossel::types::{DBResult,Pong,Inserted,Removed};
@@ -18,8 +18,13 @@ use drossel::drossel::store::*;
 use strand::mutable::{Event,AsSendableEvent};
 use strand::errors::{Errors};
 
-type ResultType = (Result<DBResult,Errors>);
-type ChannelType = (Box<Event<BinaryList, DBResult>+Send>, Sender<ResultType>);
+type DBEnvelope = Envelope<Box<Event<BinaryList, DBResult>+Send>,Result<DBResult, Errors>>;
+
+#[deriving(Send)]
+struct Envelope<M: Send, R: Send> {
+  message: M,
+  reply_to: Sender<R>
+}
 
 #[start]
 fn start(argc: int, argv: *const *const u8) -> int {
@@ -27,55 +32,79 @@ fn start(argc: int, argv: *const *const u8) -> int {
 }
 
 fn main() {
-  let (db_sender, db_receiver): (Sender<ChannelType>, Receiver<ChannelType>) = channel();
+  let db_sender = start_db();
 
+  let listener = TcpListener::bind("127.0.0.1", 7890).unwrap();
+
+  tcp_handler(listener, db_sender);
+}
+
+fn handle_stream<T: Stream>(
+    queue: Sender<DBEnvelope>,
+    mut buffer: BufferedStream<T>
+) {
+  let input = buffer.read_until('\n' as u8).unwrap();
+
+  let command: Box<Command> = get_command(input).unwrap();
+  let (sender, receiver) = channel::<Result<DBResult, Errors>>();
+  let event = (*command).as_sendable_event();
+  queue.send(Envelope { message: event, reply_to: sender });
+
+  let res = receiver.recv().unwrap();
+  let output = match res {
+    Pong                   => format!("PONG"),
+    Inserted(queue)        => format!("OK {}", queue),
+    Removed(queue, result) => format!("REMOVED {} {}", queue, result)
+  };
+
+  match write!(buffer, "{}", output) {
+    Err(e) => fail!("Failed writing to buffer: {}", e),
+    _ => {}
+  }
+  match buffer.flush() {
+    Err(e) => fail!("Failed flushing buffer: {}", e),
+    _ => {}
+  }
+}
+
+fn start_db() -> Sender<DBEnvelope> {
+  let (sender, receiver) = channel::<DBEnvelope>();
   // spawn the db task
   spawn(proc() {
     let mut db = DB::new();
-    for (event, origin) in db_receiver.iter() {
-      origin.send(db.execute(event));
+    for m in receiver.iter() {
+      m.reply_to.send(db.execute(m.message));
     }
   });
 
-  let listener = TcpListener::bind("127.0.0.1", 7890);
+  sender
+}
 
-  // bind the listener to the specified address
-  let mut acceptor = listener.listen();
+fn tcp_handler(
+    listener: TcpListener,
+    queue: Sender<DBEnvelope>,
+) {
+    let mut acceptor = listener.listen();
 
-  // accept connections and process them
-  for stream in acceptor.incoming() {
-    let cloned_sender = db_sender.clone();
-    spawn(proc() {
-      match stream {
-        Ok(conn) => {
-          let mut buffer = BufferedStream::new(conn);
-          let input = buffer.read_until('\n' as u8).unwrap();
+    for stream in acceptor.incoming() {
+        match stream {
+            Err(_) => fail!("connection error!"),
+            Ok(conn) => {
+                handler_task(&queue, conn);
+            }
+        }
+    }
 
-          let command: Box<Command> = get_command(input).unwrap();
-          let (sender, receiver): (Sender<ResultType>, Receiver<ResultType>) = channel();
-          let event = (*command).as_sendable_event();
-          cloned_sender.send((event, sender));
-          let res = receiver.recv().unwrap();
-          let output = match res {
-            Pong => { format!("PONG") },
-            Inserted(queue) => { format!("OK {}", queue) },
-            Removed(queue, result) => { format!("REMOVED {} {}", queue, result)}
-          };
-          println!("{}", output.as_slice().as_bytes() );
-          match write!(buffer, "{}", output) {
-            Err(_) => fail!("Failed writing to buffer"),
-            _ => {}
-          }
-          match buffer.flush() {
-            Err(_) => { fail!("Failed closing stream") },
-            _ => {}
-          }
-        },
-        Err(_) => { fail!("Oha?"); }
-      }
-    });
-  }
+    drop(acceptor);
+}
 
-  // close the socket server
-  drop(acceptor);
+fn handler_task(sender: &Sender<DBEnvelope>, conn: TcpStream) {
+  let cloned_sender = sender.clone();
+
+  spawn(proc() {
+      handle_stream(
+          cloned_sender,
+          BufferedStream::new(conn)
+      );
+  });
 }
